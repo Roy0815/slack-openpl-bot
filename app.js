8011fbd9f50a8ffd004a483a6e3a6d974b1ac99f
Roy0815 <roy.lotzwik@gmx.de>
2021-03-27 22:54:50 +0100

    CSV reading capabilities and test

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //This will be an App that allows Slack users to get Data from openpowerlifting.org directly into the workspace
 // Test
+
+var fs = require('fs'); 
+var parse = require('csv-parse');
+var parser = parse({columns: true}, function (err, records) {
+	console.log(records);
+});
+
+fs.createReadStream('openipf-2021.csv').pipe(parser);
