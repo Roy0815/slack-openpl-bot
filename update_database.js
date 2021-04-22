5d855d0ca2896c33989a424ce7a38d5428cfec6a
Roy0815 <roy.lotzwik@gmx.de>
2021-04-22 12:50:03 +0200

    added script for manual update of database

---
+++ update_database.js
@@ -0,0 +1,5 @@
+// this file will be used to update the database manually in case of errors
+
+const db_helper = require('./database_functions/database_helper.js')
+
+db_helper.startUpdateDatabase()
