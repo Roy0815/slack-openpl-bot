2436c4c338bb6a82d0204a173c7da545f69686c3
Roy0815 <roy.lotzwik@gmx.de>
2021-05-02 00:26:23 +0200

    upd. view concept, provided flexible build method

--- slack_functions/slack_helper.js
+++ slack_functions/slack_helper.js
@@ -3,7 +3,7 @@
 //views
 const entryMessageView = {
     channel: "",
-    text: "You mentioned me in your message, do you want to start a lookup?",
+    text: "You mentioned me in your message, do you want to start a lookup?", //this is required as a preview
     user: "",
     thread_ts: "",
     blocks: [
@@ -25,18 +25,7 @@ const entryMessageView = {
 						emoji: true
 					},
                     action_id: "entrymessage_start",
-                    style: "primary",
-					value: "click_me_123"
-				},
-				{
-					type: "button",
-					text: {
-						type: "plain_text",
-						text: "Yes and delete my message",
-						emoji: true
-					},
-                    action_id: "entrymessage_start_delete_usermessage",
-					value: "click_me_123"
+                    style: "primary"
 				},
 				{
 					type: "button",
@@ -45,85 +34,432 @@ const entryMessageView = {
 						text: "No",
 						emoji: true
 					},
-                    action_id: "entrymessage_cancel",
-					value: "click_me_123"
+                    action_id: "entrymessage_cancel"
 				}
 			]
 		}
     ]
 };
 
-const entryView = {
+const entryDialogView = {
     trigger_id: '',
-        view: {
-            type: "modal",
-            title: {
-                type: "plain_text",
-                text: "Openpowerlifting Bot"
-            },
-            blocks: [
-                {
-                    type: "section",
-                    text: {
-                        type: "mrkdwn",
-                        text: "Please choose the action you want to take"
-                    }
+    view: {
+        type: "modal",
+        callback_id: "entrydialog",
+        submit: {
+            type: "plain_text",
+            text: "Submit",
+            emoji: true
+        },
+        close: {
+            type: "plain_text",
+            text: "Cancel",
+            emoji: true
+        },
+        title: {
+            type: "plain_text",
+            text: "Openpowerlifting Bot"
+        },
+        blocks: [
+            {
+                type: "section",
+                block_id: "entrydialog_radiobuttons",
+                text: {
+                    type: "mrkdwn",
+                    text: "Please choose the action you want to take"
                 },
-                {
-                    type: "actions",
-                    elements: [
+                accessory: {
+                    type: "radio_buttons",
+                    action_id: "entrydialog_radiobuttons",
+                    options: [
                         {
-                            type: "button",
                             text: {
                                 type: "plain_text",
-                                text: "Last meet",
+                                text: "Last Meet",
                                 emoji: true
                             },
-                            value: "click_me_123",
-                            action_id: "actionId-0"
+                            value: "lastmeet"
                         },
                         {
-                            type: "button",
                             text: {
                                 type: "plain_text",
                                 text: "Best Meet",
                                 emoji: true
                             },
-                            value: "click_me_123",
-                            action_id: "actionId-1"
+                            value: "bestmeet"
                         },
                         {
-                            type: "button",
                             text: {
                                 type: "plain_text",
                                 text: "Compare",
                                 emoji: true
                             },
-                            value: "click_me_123",
-                            action_id: "actionId-2"
+                            value: "compare"
                         },
                         {
-                            type: "button",
                             text: {
                                 type: "plain_text",
                                 text: "Top 10 by criteria",
                                 emoji: true
                             },
-                            value: "click_me_123",
-                            action_id: "actionId-3"
+                            value: "top10"
                         }
                     ]
                 }
+            }
+        ]
+    }
+};
+
+const lastmeetSubView = [
+    {
+        type: "divider"
+    },
+    {
+        type: "input",
+        element: {
+            type: "plain_text_input",
+            action_id: "lastmeet_person_input",
+            placeholder: {
+                type: "plain_text",
+                text: "Firstname Lastname [#]",
+                emoji: true
+            }
+        },
+        label: {
+            type: "plain_text",
+            text: "Person",
+            emoji: true
+        }
+    }
+];
+
+const bestmeetSubView = [
+    {
+        type: "divider"
+    },
+    {
+        type: "input",
+        element: {
+            type: "plain_text_input",
+            action_id: "bestmeet_person_input",
+            placeholder: {
+                type: "plain_text",
+                text: "Firstname Lastname [#]",
+                emoji: true
+            }
+        },
+        label: {
+            type: "plain_text",
+            text: "Person",
+            emoji: true
+        }
+    },
+    {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "By criteria"
+        },
+        accessory: {
+            type: "static_select",
+            action_id: "bestmeet_criteria_input",
+            initial_option: {
+                text: {
+                    type: "plain_text",
+                    text: "Dots",
+                    emoji: true
+                },
+                value: "dots"
+            },
+            options: [
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Total",
+                        emoji: true
+                    },
+                    value: "total"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Wilks",
+                        emoji: true
+                    },
+                    value: "wilks"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Dots",
+                        emoji: true
+                    },
+                    value: "dots"
+                }
             ]
-        }   
+        }
+    }
+];
+
+const compareSubView = [
+    {
+        type: "divider"
+    },
+    {
+        type: "input",
+        element: {
+            type: "plain_text_input",
+            action_id: "compare_person1_input",
+            placeholder: {
+                type: "plain_text",
+                text: "Firstname Lastname [#]",
+                emoji: true
+            }
+        },
+        label: {
+            type: "plain_text",
+            text: "Person 1",
+            emoji: true
+        }
+    },
+    {
+        type: "input",
+        element: {
+            type: "plain_text_input",
+            action_id: "compare_person2_input",
+            placeholder: {
+                type: "plain_text",
+                text: "Firstname Lastname [#]",
+                emoji: true
+            }
+        },
+        label: {
+            type: "plain_text",
+            text: "Person 2",
+            emoji: true
+        }
+    },
+    {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "By criteria"
+        },
+        accessory: {
+            type: "static_select",
+            action_id: "compare_criteria_input",
+            initial_option: {
+                text: {
+                    type: "plain_text",
+                    text: "Dots",
+                    emoji: true
+                },
+                value: "dots"
+            },
+            options: [
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Total",
+                        emoji: true
+                    },
+                    value: "total"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Wilks",
+                        emoji: true
+                    },
+                    value: "wilks"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Dots",
+                        emoji: true
+                    },
+                    value: "dots"
+                }
+            ]
+        }
+    }
+];
+
+const top10SubView = [
+    {
+        type: "divider"
+    },
+    {
+        type: "input",
+        element: {
+            type: "plain_text_input",
+            action_id: "top10_meet_input",
+            placeholder: {
+                type: "plain_text",
+                text: "optional",
+                emoji: true
+            }
+        },
+        label: {
+            type: "plain_text",
+            text: "Meet Name",
+            emoji: true
+        }
+    },
+    {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "Compare method"
+        },
+        accessory: {
+            type: "static_select",
+            action_id: "top10_comparemethod_input",
+            initial_option: {
+                text: {
+                    type: "plain_text",
+                    text: "Dots",
+                    emoji: true
+                },
+                value: "dots"
+            },
+            options: [
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Total",
+                        emoji: true
+                    },
+                    value: "total"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Wilks",
+                        emoji: true
+                    },
+                    value: "wilks"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Dots",
+                        emoji: true
+                    },
+                    value: "dots"
+                }
+            ]
+        }
+    },
+    {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "Gender"
+        },
+        accessory: {
+            type: "static_select",
+            action_id: "top10_gender_input",
+            placeholder: {
+                type: "plain_text",
+                text: "optional",
+                emoji: true
+            },
+            options: [
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Male",
+                        emoji: true
+                    },
+                    value: "m"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Female",
+                        emoji: true
+                    },
+                    value: "f"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Mx",
+                        emoji: true
+                    },
+                    value: "mx"
+                }
+            ]
+        }
+    },
+    {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "Tested"
+        },
+        accessory: {
+            type: "static_select",
+            action_id: "top10_tested_input",
+            placeholder: {
+                type: "plain_text",
+                text: "optional",
+                emoji: true
+            },
+            options: [
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "Yes",
+                        emoji: true
+                    },
+                    value: "tested"
+                },
+                {
+                    text: {
+                        type: "plain_text",
+                        text: "No",
+                        emoji: true
+                    },
+                    value: "untested"
+                }
+            ]
+        }
+    }
+];
+
+//view builder functions
+function getEntryDialog(subviewName) {
+    let baseView = JSON.parse(JSON.stringify(entryDialogView));
+
+    switch (subviewName){
+        case 'lastmeet':
+            subView = lastmeetSubView;
+            break;
+        case 'bestmeet':
+            subView = bestmeetSubView;
+            break;
+        case 'compare':
+            subView = compareSubView;
+            break;
+        case 'top10':
+            subView = top10SubView;
+            break;
+        default:
+            return baseView;
+    }
+
+    baseView.view.blocks = baseView.view.blocks.concat(subView);
+    console.log(baseView.view.blocks);
+    console.log(entryDialogView.view.blocks);
+    return baseView;
 }
 
 //exports
 module.exports = {
-    sendMessage : function (text, channel) {
-        console.log('Test')
-    },
-
-    entryView,
+    getEntryDialog,
     entryMessageView
 };
