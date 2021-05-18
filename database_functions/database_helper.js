9b0fcab35ff4b4c9a027252aca82274f5d0af6a6
Roy0815 <roy.lotzwik@gmx.de>
2021-05-19 00:10:19 +0200

    formatted and added selectUser function

--- database_functions/database_helper.js
+++ database_functions/database_helper.js
@@ -7,77 +7,88 @@ const { Pool } = require('pg')
 
 const csvPath = '/var/lib/files/openpowerlifting-latest.csv'
 const zipPath = '/var/lib/files/openpowerlifting-latest.zip'
-const openPLurl = 'https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip'
+const openPLurl =
+    'https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip'
 
 module.exports = {
-    startUpdateDatabase : function () {
+    startUpdateDatabase: function () {
         console.log('Database update started')
         downloadZip()
-    }
-};
+    },
+    selectUser
+}
 
 function unzipFolder() {
-    fs.createReadStream(zipPath).pipe(unzip.Parse())
-    .on('entry', (entry) => {
-        if (entry.type == 'File' && entry.path.endsWith('.csv')) { //exclude Folders and the .txt Files
-            entry.pipe(fs.createWriteStream(csvPath)) //CSV found --> save to system
-        } else {
-            entry.autodrain() //leave the ones we don't need
-        }
-    })
-    .on('finish', () => {
-        fs.unlink(zipPath, (err) => { if (err) { console.log(err) } }) // delete ZIP
-        updateDatabase()
-        console.log('Unzip finished, Zip deleted')
-    }); 
+    fs.createReadStream(zipPath)
+        .pipe(unzip.Parse())
+        .on('entry', (entry) => {
+            if (entry.type == 'File' && entry.path.endsWith('.csv')) {
+                //exclude Folders and the .txt Files
+                entry.pipe(fs.createWriteStream(csvPath)) //CSV found --> save to system
+            } else {
+                entry.autodrain() //leave the ones we don't need
+            }
+        })
+        .on('finish', () => {
+            fs.unlink(zipPath, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+            }) // delete ZIP
+            updateDatabase()
+            console.log('Unzip finished, Zip deleted')
+        })
 }
 
 function downloadZip() {
-    https.get(openPLurl, function(response) {
-        switch(response.statusCode) {
-            case 200:
-                let file = fs.createWriteStream(zipPath); //save file
-                response.on('data', function(chunk){
-                    file.write(chunk)
-                }).on('end', function(){
-                    file.end()
-                    unzipFolder(zipPath) //downloading done -> extract data
-                    console.log('Download of Zip finished')
-                });
-                break;
-            default:
-                console.log('An Error occured while downloading')
-        }
-    }).on("error", (err) => {
-        console.log('An Error occured while downloading')
-    })
+    https
+        .get(openPLurl, function (response) {
+            switch (response.statusCode) {
+                case 200:
+                    let file = fs.createWriteStream(zipPath) //save file
+                    response
+                        .on('data', function (chunk) {
+                            file.write(chunk)
+                        })
+                        .on('end', function () {
+                            file.end()
+                            unzipFolder(zipPath) //downloading done -> extract data
+                            console.log('Download of Zip finished')
+                        })
+                    break
+                default:
+                    console.log('An Error occured while downloading')
+            }
+        })
+        .on('error', (err) => {
+            console.log('An Error occured while downloading')
+        })
 }
 
 function updateDatabase() {
-    console.log("Database query started")
+    console.log('Database query started')
 
     let pool = new Pool({
         user: process.env.DB_POSTGRES_USER,
         host: process.env.DB_POSTGRES_HOST,
         database: process.env.DB_POSTGRES_DATABASE,
         password: process.env.DB_POSTGRES_PASSWORD,
-        port: process.env.DB_POSTGRES_PORT,
-    });
+        port: process.env.DB_POSTGRES_PORT
+    })
 
-    let query = 'CREATE TEMP TABLE lifterdata_csv_temp ON COMMIT DROP ' +
-                'AS SELECT * FROM public.lifterdata_csv WITH NO DATA;' +
-
-                'COPY lifterdata_csv_temp (name, sex, event, equipment, age, ageclass, birthyearclass, division, bodyweightkg, weightclasskg, squat1kg, squat2kg, squat3kg, squat4kg, best3squatkg, bench1kg, bench2kg, bench3kg, bench4kg, best3benchkg, deadlift1kg, deadlift2kg, deadlift3kg, deadlift4kg, best3deadliftkg, totalkg, place, dots, wilks, glossbrenner, goodlift, tested, country, state, federation, parentfederation, date, meetcountry, meetstate, meettown, meetname) ' +
-                'FROM \'' + csvPath + '\'' + 'DELIMITER \',\' CSV HEADER QUOTE \'"\' ' +
-                'ESCAPE \'\'\'\' FORCE NOT NULL bodyweightkg, totalkg, division;' +
-                
-                'TRUNCATE public.lifterdata_csv;' +
-
-                'INSERT INTO public.lifterdata_csv ' +
-                'SELECT * FROM lifterdata_csv_temp ON CONFLICT DO NOTHING;';
+    let query =
+        'CREATE TEMP TABLE lifterdata_csv_temp ON COMMIT DROP ' +
+        'AS SELECT * FROM public.lifterdata_csv WITH NO DATA;' +
+        'COPY lifterdata_csv_temp (name, sex, event, equipment, age, ageclass, birthyearclass, division, bodyweightkg, weightclasskg, squat1kg, squat2kg, squat3kg, squat4kg, best3squatkg, bench1kg, bench2kg, bench3kg, bench4kg, best3benchkg, deadlift1kg, deadlift2kg, deadlift3kg, deadlift4kg, best3deadliftkg, totalkg, place, dots, wilks, glossbrenner, goodlift, tested, country, state, federation, parentfederation, date, meetcountry, meetstate, meettown, meetname) ' +
+        `FROM '${csvPath}'` +
+        "DELIMITER ',' CSV HEADER QUOTE '\"' " +
+        "ESCAPE '''' FORCE NOT NULL bodyweightkg, totalkg, division;" +
+        'TRUNCATE public.lifterdata_csv;' +
+        'INSERT INTO public.lifterdata_csv ' +
+        'SELECT * FROM lifterdata_csv_temp ON CONFLICT DO NOTHING;'
 
     pool.connect((err, client, done) => {
-        if (err) throw err;
+        if (err) throw err
         try {
             client.query(query, (err, res) => {
                 if (err) {
@@ -87,12 +98,52 @@ function updateDatabase() {
                     console.log('Database updated!')
                     console.log(res?.[4]) //only log the insert
                 }
-                fs.unlink(csvPath, (err) => { if (err) { console.log(err) } }) // delete CSV
+                fs.unlink(csvPath, (err) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                }) // delete CSV
                 console.log('CSV deleted')
                 done()
             })
         } finally {
             //done()
         }
+    })
+}
+
+function selectUser(name) {
+    let pool = new Pool({
+        user: process.env.DB_POSTGRES_USER,
+        host: process.env.DB_POSTGRES_HOST,
+        database: process.env.DB_POSTGRES_DATABASE,
+        password: process.env.DB_POSTGRES_PASSWORD,
+        port: process.env.DB_POSTGRES_PORT
+    })
+
+    let namePattern = buildNamePattern(name)
+
+    let query =
+        'SELECT DISTINCT ON (name) name, date, meetname, division, weightclasskg, place, dots, best3squatkg, best3benchkg, best3deadliftkg, totalkg ' +
+        'FROM public.lifterdata_csv ' +
+        `WHERE name LIKE '${namePattern}' ` +
+        'ORDER BY name ASC, date DESC;'
+
+    pool.connect((err, client, done) => {
+        if (err) throw err
+        client.query(query, (err, res) => {
+            if (err) {
+                console.log('Error in Database query')
+                console.log(err.stack)
+            } else {
+                console.log('Select successful')
+                console.log(res)
+            }
+            done()
         })
+    })
+}
+
+function buildNamePattern(name) {
+    return `%${name.split(' ').join('%')}%`
 }
