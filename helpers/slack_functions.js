8e659006ea173aebb0a184d49e7e5735764c99a1
Roy0815 <roy.lotzwik@gmx.de>
2021-05-21 21:28:14 +0200

    moved export to begin and splitted result funcs

--- helpers/slack_functions.js
+++ helpers/slack_functions.js
@@ -1,6 +1,23 @@
 // file with everything for slack interactions
 const slack_views = require('./slack_views')
 
+//exports
+module.exports = {
+    getEntryDialog,
+    getResultMessage,
+    getLastmeetResult,
+    getBestmeetResult,
+    getCompareResult,
+    getRankingLink,
+    getMeetLink,
+    commandDialog,
+    commandLastmeet,
+    commandBestmeet,
+    commandCompare,
+    commandMeetlink,
+    commandRanking
+}
+
 //functions
 const commandDialog = 'openpl'
 const commandLastmeet = 'lastmeet'
@@ -35,68 +52,88 @@ function getEntryDialog(subviewName) {
     return baseView
 }
 
-function getResultMessage(channel, option, data) {
-    let resultMessage
+function getLastmeetResult(channel, person) {
+    let resultMessage = JSON.parse(
+        JSON.stringify(slack_views.singlemeetResultMessageView)
+    )
+    resultMessage.text = `Last meet of *${person.name}*` //message preview
+    resultMessage.blocks[0].text.text = `Last meet of *${person.name}* <https://www.openpowerlifting.org/u/roylotzwik|openpowerlifting.org>`
 
-    switch (option) {
-        case commandLastmeet:
-            resultMessage = JSON.parse(
-                JSON.stringify(slack_views.singlemeetResultMessageView)
-            )
-            resultMessage.text = `Last meet of *${data[0].name}*`
-            resultMessage.blocks[0].text.text = `Last meet of *${data[0].name}* <https://www.openpowerlifting.org/u/roylotzwik|openpowerlifting.org>`
+    resultMessage.blocks[0].fields[0].text = `*Meet:* ${person.meetname}`
+    resultMessage.blocks[0].fields[1].text = `*Date:* ${person.date}`
 
-            resultMessage.blocks[0].fields[0].text = `*Meet:* ${data[0].meetname}`
-            resultMessage.blocks[0].fields[1].text = `*Date:* ${data[0].date}`
+    resultMessage.blocks[1].fields[0].text = `*Categorie:* ${person.division}`
+    resultMessage.blocks[1].fields[1].text = `*Class:* ${person.weightclasskg}`
+    resultMessage.blocks[1].fields[2].text = `*Place:* ${person.place}`
+    resultMessage.blocks[1].fields[3].text = `*Dots:* ${person.dots}`
 
-            resultMessage.blocks[1].fields[0].text = `*Categorie:* ${data[0].division}`
-            resultMessage.blocks[1].fields[1].text = `*Class:* ${data[0].weightclasskg}`
-            resultMessage.blocks[1].fields[2].text = `*Place:* ${data[0].place}`
-            resultMessage.blocks[1].fields[3].text = `*Dots:* ${data[0].dots}`
-
-            resultMessage.blocks[2].fields[0].text = `*Squat:* ${data[0].best3squatkg}`
-            resultMessage.blocks[2].fields[1].text = `*Bench:* ${data[0].best3benchkg}`
-            resultMessage.blocks[2].fields[2].text = `*Deadlift:* ${data[0].best3deadliftkg}`
-            resultMessage.blocks[2].fields[3].text = `*Total:* ${data[0].totalkg}`
-            break
-        case commandBestmeet:
-            resultMessage = JSON.parse(
-                JSON.stringify(slack_views.singlemeetResultMessageView)
-            )
-            resultMessage.text = 'Best meet of *Roy Lotzwik*'
-            resultMessage.blocks[0].text.text =
-                'Best meet of *Roy Lotzwik* <https://www.openpowerlifting.org/u/roylotzwik|openpowerlifting.org>'
-            break
-        case commandCompare:
-            resultMessage = JSON.parse(
-                JSON.stringify(slack_views.compareResultMessageView)
-            )
-            break
-        case commandRanking:
-            resultMessage = JSON.parse(
-                JSON.stringify(slack_views.singlemeetResultMessageView)
-            )
-            break
-        default:
-            resultMessage = JSON.parse(
-                JSON.stringify(slack_views.singlemeetResultMessageView)
-            )
-            break
-    }
+    resultMessage.blocks[2].fields[0].text = `*Squat:* ${person.best3squatkg}`
+    resultMessage.blocks[2].fields[1].text = `*Bench:* ${person.best3benchkg}`
+    resultMessage.blocks[2].fields[2].text = `*Deadlift:* ${person.best3deadliftkg}`
+    resultMessage.blocks[2].fields[3].text = `*Total:* ${person.totalkg}`
 
     resultMessage.channel = channel
-
     return resultMessage
 }
 
-//exports
-module.exports = {
-    getEntryDialog,
-    getResultMessage,
-    commandDialog,
-    commandLastmeet,
-    commandBestmeet,
-    commandCompare,
-    commandMeetlink,
-    commandRanking
+function getBestmeetResult(channel, person) {
+    let resultMessage = JSON.parse(
+        JSON.stringify(slack_views.singlemeetResultMessageView)
+    )
+    resultMessage.text = `Best meet of ${person.name}` //message preview
+    resultMessage.blocks[0].text.text = `Best meet of *${person.name}* <https://www.openpowerlifting.org/u/roylotzwik|openpowerlifting.org>`
+
+    resultMessage.blocks[0].fields[0].text = `*Meet:* ${person.meetname}`
+    resultMessage.blocks[0].fields[1].text = `*Date:* ${person.date}`
+
+    resultMessage.blocks[1].fields[0].text = `*Categorie:* ${person.division}`
+    resultMessage.blocks[1].fields[1].text = `*Class:* ${person.weightclasskg}`
+    resultMessage.blocks[1].fields[2].text = `*Place:* ${person.place}`
+    resultMessage.blocks[1].fields[3].text = `*Dots:* ${person.dots}`
+
+    resultMessage.blocks[2].fields[0].text = `*Squat:* ${person.best3squatkg}`
+    resultMessage.blocks[2].fields[1].text = `*Bench:* ${person.best3benchkg}`
+    resultMessage.blocks[2].fields[2].text = `*Deadlift:* ${person.best3deadliftkg}`
+    resultMessage.blocks[2].fields[3].text = `*Total:* ${person.totalkg}`
+
+    resultMessage.channel = channel
+    return resultMessage
+}
+
+async function getCompareResult(command) {
+    let resultMessage = JSON.parse(
+        JSON.stringify(slack_views.compareResultMessageView)
+    )
+    resultMessage.text = `Comparison of ${persons[0].name} and ${persons[1].name}` //message preview
+    resultMessage.blocks[0].text.text = `Comparison of <https://www.openpowerlifting.org/u/roylotzwik|${persons[0].name}> and <https://www.openpowerlifting.org/u/simondanieloswald|${persons[1].name}>`
+
+    resultMessage.blocks[2].text.text = `*${persons[0].name}* personal best:`
+    resultMessage.blocks[2].fields[0].text = `*Date:* ${persons[0].date}`
+    resultMessage.blocks[2].fields[1].text = `*Bodyweight:* ${persons[0].bodyweightkg} kg`
+    resultMessage.blocks[2].fields[2].text = `*Dots:* ${persons[0].dots}`
+    resultMessage.blocks[2].fields[3].text = `*Total:* ${persons[0].totalkg} kg`
+
+    resultMessage.blocks[3].fields[0].text = `*Squat:* ${persons[1].best3squatkg} kg`
+    resultMessage.blocks[3].fields[1].text = `*Bench:* ${persons[1].best3benchkg} kg`
+    resultMessage.blocks[3].fields[2].text = `*Deadlift:* ${persons[1].best3deadliftkg} kg`
+
+    resultMessage.blocks[6].text.text = `*${persons[1].name}* personal best:`
+    resultMessage.blocks[6].fields[0].text = `*Date:* ${persons[1].date}`
+    resultMessage.blocks[6].fields[1].text = `*Bodyweight:* ${persons[1].bodyweightkg} kg`
+    resultMessage.blocks[6].fields[2].text = `*Dots:* ${persons[1].dots}`
+    resultMessage.blocks[6].fields[3].text = `*Total:* ${persons[1].totalkg} kg`
+
+    resultMessage.blocks[7].fields[0].text = `*Squat:* ${persons[1].best3squatkg} kg`
+    resultMessage.blocks[7].fields[1].text = `*Bench:* ${persons[1].best3benchkg} kg`
+    resultMessage.blocks[7].fields[2].text = `*Deadlift:* ${persons[1].best3deadliftkg} kg`
+
+    resultMessage.blocks[9].text.text = `*${persons[1].name} wins by 82,5kg Total!*`
+}
+
+function getRankingLink(filters) {
+    return '<https://www.openpowerlifting.org/rankings/ipf93/bvdk/men/20-23|-93 kg, men, Juniors, BVDK>'
+}
+
+function getMeetLink(meetname) {
+    return '<https://www.openpowerlifting.org/m/bvdk/1938|2019 BVDK BWG KDK Classic>'
 }
