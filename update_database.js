585367e464fcd8f1c4085470d5b8a83cf7020250
Roy0815 <roy.lotzwik@gmx.de>
2023-03-18 00:26:08 +0100

    fixed import

--- update_database.js
+++ update_database.js
@@ -1,6 +1,6 @@
 // this file will be used to update the database manually in case of errors
 
-const db_helper = require("./database_functions/database_helper.js");
+const db_helper = require("./helpers/database_functions.js");
 
 console.log("update_database.js started");
 db_helper.startUpdateDatabase();
